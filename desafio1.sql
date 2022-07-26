ccc1d0bed076b39f522a4af6662d6d99c90eaf7e
adevmir <ademirvalonqar@gmail.com>
2022-07-26 10:47:20 -0300

    [Requisito 06] - Seleciona os preços minimos e maximos dos planos, além da media e da soma de planos assinados pelos usuarios

--- desafio1.sql
+++ desafio1.sql
@@ -4,7 +4,7 @@ CREATE DATABASE SpotifyClone;
 
 CREATE TABLE SpotifyClone.plans(
     planName VARCHAR(255),
-    price DECIMAL(4,2),
+    price DECIMAL(4,2) NOT NULL,
     planCode INT AUTO_INCREMENT PRIMARY KEY
 ) engine = InnoDB;
 
@@ -133,15 +133,15 @@ VALUES
 INSERT INTO SpotifyClone.users (userName, idade, planCode, dateSignature)
 VALUES
   ('Thati', 23, 1, '2019-10-20'),
-  ('Cintia', 35, 2, '2017-12-30'),
-  ('Bill', 20, 3, '2019-06-05'),
-  ('Roger', 45, 4, '2020-05-13'),
-  ('Norman', 58, 4, '2017-02-17'),
-  ('Patrick', 33, 2, '2017-01-06'),
-  ('Vivian', 26, 3, '2018-01-05'),
-  ('Carol', 19, 3, '2018-02-14'),
-  ('Angelina', 42, 2, '2018-04-29'),
-  ('Paul', 46, 2, '2017-01-17');
+  ('Cintia', 35, 4, '2017-12-30'),
+  ('Bill', 20, 2, '2019-06-05'),
+  ('Roger', 45, 3, '2020-05-13'),
+  ('Norman', 58, 3, '2017-02-17'),
+  ('Patrick', 33, 4, '2017-01-06'),
+  ('Vivian', 26, 2, '2018-01-05'),
+  ('Carol', 19, 2, '2018-02-14'),
+  ('Angelina', 42, 4, '2018-04-29'),
+  ('Paul', 46, 4, '2017-01-17');
 
 INSERT INTO SpotifyClone.historic (userID, musicID,  dateReproduction)
 VALUES
